d8e1854b72b98421b2eb41ba8240b2847e7ef4a3
James Galantino <james_galantino@aol.com>
2021-01-21 21:40:29 -0500

    Migrated code from index.html to script.js

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
-console.log('hey');
+// Display time at top of page
+$('#currentDay').text(moment().format("dddd, MMMM Do YYYY, h:mm a"))
+
+var timeBlocks = [
+  {
+    hour: '9:00 AM',
+    event: ""
+  },
+  {
+    hour: '10:00 AM',
+    event: ""
+  },
+  {
+    hour: '11:00 AM',
+    event: ""
+  },
+  {
+    hour: '12:00 PM',
+    event: ""
+  },
+  {
+    hour: '1:00 PM',
+    event: ""
+  },
+  {
+    hour: '2:00 PM',
+    event: ""
+  },
+  {
+    hour: '3:00 PM',
+    event: ""
+  },
+  {
+    hour: '4:00 PM',
+    event: ""
+  },
+  {
+    hour: '5:00 PM',
+    event: ""
+  },
+];
+
+// timeblock linked to time displayed
+// timeblock changes color based on current time
+  // past? 
+  // current?
+  // future?
+
+// save button will store event in local storage
