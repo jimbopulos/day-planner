20db09fea3234f24e443f533b40d287f7a01dc78
James Galantino <james_galantino@aol.com>
2021-01-21 22:25:17 -0500

    Added local storage function in script.js. Changed elements within object timeBlocks

--- script.js
+++ script.js
@@ -3,49 +3,72 @@ $('#currentDay').text(moment().format("dddd, MMMM Do YYYY, h:mm a"))
 
 var timeBlocks = [
   {
-    hour: '9:00 AM',
-    event: ""
+    hour: '9AM',
+    appt: 'inputAppt.value.trim()'
   },
   {
-    hour: '10:00 AM',
-    event: ""
+    hour: '10AM',
+    appt: ""
   },
   {
-    hour: '11:00 AM',
-    event: ""
+    hour: '11AM',
+    appt: ""
   },
   {
-    hour: '12:00 PM',
-    event: ""
+    hour: '12PM',
+    appt: ""
   },
   {
-    hour: '1:00 PM',
-    event: ""
+    hour: '1PM',
+    appt: ""
   },
   {
-    hour: '2:00 PM',
-    event: ""
+    hour: '2PM',
+    appt: ""
   },
   {
-    hour: '3:00 PM',
-    event: ""
+    hour: '3PM',
+    appt: ""
   },
   {
-    hour: '4:00 PM',
-    event: ""
+    hour: '4PM',
+    appt: ""
   },
   {
-    hour: '5:00 PM',
-    event: ""
+    hour: '5PM',
+    appt: ""
   },
 ];
+
+console.log(timeBlocks);
 
 // save button will store event in local storage
 // event listener for save button
 $('button').on('click', function () {
-    // input sends to locals storage
-    textArea.val().localStorage.getItem('event');
-})
+    // set input
+    localStorage.setItem('timeBlock', JSON.stringify(timeBlocks));
+    // get input
+    var getEvent = JSON.parse(localStorage.getItem('timeBlock'));
+    // textArea.val().localStorage.getItem('event');
+});
+
+// function saveLastGrade() {
+//     // Save related form data as an object
+//     var studentGrade = {
+//       student: student.value,
+//       grade: grade.value,
+//       comment: comment.value.trim()
+//     };
+//     // Use .setItem() to store object in storage and JSON.stringify to convert it as a string
+//     localStorage.setItem("studentGrade", JSON.stringify(studentGrade));
+//   }
+
+//   function saveEvent() {
+//     // Save even input data as an object
+//     localStorage.setItem('timeBlock', JSON.stringify(timeBlocks));
+//   }
+
+// place in local storage
 
 // timeblock linked to time displayed
 // timeblock changes color based on current time
